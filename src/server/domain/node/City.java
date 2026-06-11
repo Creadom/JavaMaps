22e628ba3fc4c4c96b260202d478a66c74eb5279
creadom <florian.rdouit@gmail.com>
2026-06-11 10:53:30 +0200

    Add City and Road classes to represent needed entities

---
+++ src/server/domain/node/City.java
@@ -0,0 +1,13 @@
+package server.domain.node;
+
+public class City {
+    public final String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/server/domain/node/Road.java
@@ -0,0 +1,20 @@
+package server.domain.node;
+
+public class Road {
+    private final City destination;
+    private int travelTimeMinutes; //time in minutes
+
+    public Road(City destination, int travelTimeMinutes)
+    {
+        this.destination = destination;
+        this.travelTimeMinutes = travelTimeMinutes;
+    }
+
+    public int getTravelTimeMinutes() {
+        return travelTimeMinutes;
+    }
+
+    public void setTravelTimeMinutes(int travelTimeMinutes) {
+        this.travelTimeMinutes = travelTimeMinutes;
+    }
+}
