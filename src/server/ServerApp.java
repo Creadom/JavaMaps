4394c7fef24a273a5722c449167fa2a0811d0da8
creadom <florian.rdouit@gmail.com>
2026-06-11 11:09:30 +0200

    Add example usage of RoutingGraph in ServerApp with sample cities and roads

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -1,7 +1,26 @@
 package server;
 
+import server.domain.model.*;
+
 public class ServerApp {
     static void main(String[] args) {
         System.out.println("Hello World!");
+
+        City sion     = new City("Sion");
+        City sierre   = new City("Sierre");
+        City stLeo    = new City("St-Léonard");
+
+        RoutingGraph graph = new RoutingGraph(3);
+        graph.addCity(0, sion);
+        graph.addCity(1, stLeo);
+        graph.addCity(2, sierre);
+
+        graph.addRoad(sion, stLeo, 8);
+        graph.addRoad(stLeo, sierre, 7);
+        graph.addRoad(sion, sierre, 20);
+
+        for (Road r : graph.getRoadsFrom("Sion")) {
+            System.out.println("→ " + r.getDestination().getName() + " " + r.getTravelTimeMinutes() + " min");
+        }
     }
 }
