fc94f05226a5781be9b99ff2639d928ae3338083
creadom <florian.rdouit@gmail.com>
2026-06-11 16:37:33 +0200

    feat: add RouteCommand
    Builds OK|ROUTE|<eta>|<city...> reply; returns ERR for unknown cities

---
+++ src/server/net/command/RouteCommand.java
@@ -0,0 +1,38 @@
+package server.net.command;
+
+import common.Protocol;
+import server.domain.model.City;
+import server.domain.model.RouteResult;
+import server.net.RequestContext;
+
+public class RouteCommand implements ServerCommand {
+
+    private final String from;
+    private final String to;
+
+    public RouteCommand(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String execute(RequestContext ctx) {
+        if (!ctx.getGraph().hasCity(from)) {
+            return Protocol.ERR + Protocol.SEPARATOR + "Unknown city '" + from + "'";
+        }
+        if (!ctx.getGraph().hasCity(to)) {
+            return Protocol.ERR + Protocol.SEPARATOR + "Unknown city '" + to + "'";
+        }
+
+        RouteResult result = ctx.getRoutingService().findRoute(ctx.getGraph(), from, to);
+
+        StringBuilder reply = new StringBuilder();
+        reply.append(Protocol.OK)
+             .append(Protocol.SEPARATOR).append(Protocol.CMD_ROUTE)
+             .append(Protocol.SEPARATOR).append(result.getTotalTimeMinutes());
+        for (City city : result.getPath()) {
+            reply.append(Protocol.SEPARATOR).append(city.getName());
+        }
+        return reply.toString();
+    }
+}
